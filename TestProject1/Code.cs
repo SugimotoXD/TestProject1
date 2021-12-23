3e7ce37ad5bc2c3aba7c242bc217a21d8a58a72a
fsafdsa Lfybkf <P.danila228@mail.ru>
2021-12-23 10:47:39 +0200

    Добавьте файлы проекта.

---
+++ TestProject1/Code.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestLaba1i3
+{
+    public class Code
+    {
+        static void Main(string[] args)
+        {
+            Atmosfera auto = new Atmosfera("Атмосфера");
+            Luna auto2 = new Luna("Луна");
+            Noch auto3 = new Noch("Ночь");
+            Artur auto4 = new Artur("Артур");
+            Starik auto5 = new Starik("Старик");
+
+            Console.WriteLine(" {1}  {3}  {5}  {7} {9}", auto.name, auto.Zadacha(), auto2.name, auto2.Zadacha2(), auto3.name, auto3.Zadacha3(), auto4.name, auto4.Zadacha4(), auto5.name, auto5.Zadacha5());
+            Console.ReadKey();
+        }
+    }
+
+    public class Atmosfera
+    {
+        public string name { get; set; }
+        public Atmosfera(string name)
+        {
+            this.name = name;
+        }
+
+        public string Zadacha()
+        {
+            return "Iz-za razrajennosti atmosferi";
+        }
+    }
+
+    public class Luna
+    {
+        public string name { get; set; }
+        public Luna(string name)
+        {
+            this.name = name;
+        }
+
+        public string Zadacha2()
+        {
+            return "i iz-za togo chto luni ne bilo";
+        }
+    }
+
+    public class Noch
+    {
+        public string name { get; set; }
+        public Noch(string name)
+        {
+            this.name = name;
+        }
+
+        public string Zadacha3()
+        {
+            return "Noch nastupila ochen bistro, i bilo yje ochen temno.";
+        }
+    }
+
+    public class Artur
+    {
+        public string name { get; set; }
+        public Artur(string name)
+        {
+            this.name = name;
+        }
+
+        public string Zadacha4()
+        {
+            return "Po etomy Atrur ne zametil";
+        }
+    }
+    public class Starik
+    {
+        public string name { get; set; }
+        public Starik(string name)
+        {
+            this.name = name;
+        }
+
+        public string Zadacha5()
+        {
+            return "Starika, poka ne stolknulsa s nim";
+        }
+    }
+}
+
---
+++ UnitTestProject1/TestPLW13.cs
@@ -0,0 +1,92 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using TestLaba1i3;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class TestPLW13
+    {
+        [TestMethod]
+        public void TestMethod()
+        {
+            Atmosfera auto = new Atmosfera("Атмосфера");
+            Luna auto2 = new Luna("Луна");
+
+            Assert.AreEqual("Атмосфера", auto.name);
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            Atmosfera auto = new Atmosfera("Атмосфера");
+            Assert.AreEqual("Iz-za razrajennosti atmosferi", auto.Zadacha());
+        }
+
+        [TestMethod]
+        public void TestMethod3()
+        {
+            Luna auto2 = new Luna("Луна");
+            Noch auto3 = new Noch("Ночь");
+
+            Assert.AreEqual("Луна", auto2.name);
+        }
+
+        [TestMethod]
+        public void TestMethod4()
+        {
+            Luna auto2 = new Luna("Луна");
+            Assert.AreEqual("i iz-za togo chto luni ne bilo", auto2.Zadacha2());
+        }
+
+        [TestMethod]
+        public void TestMethod5()
+        {
+            Noch auto3 = new Noch("Ночь");
+            Artur auto4 = new Artur("Скорость");
+            Starik auto5 = new Starik("Старик");
+
+            Assert.AreEqual("Ночь", auto3.name);
+        }
+
+        [TestMethod]
+        public void TestMethod6()
+        {
+            Noch auto3 = new Noch("Ночь");
+            Assert.AreEqual("Noch nastupila ochen bistro, i bilo yje ochen temno.", auto3.Zadacha3());
+        }
+
+        [TestMethod]
+        public void TestMethod7()
+        {
+            Artur auto4 = new Artur("Артур");
+            Assert.AreEqual(auto4.name, "Артур");
+        }
+
+        [TestMethod]
+        public void TestMothod8()
+        {
+            Artur auto4 = new Artur("Артур");
+            Assert.AreEqual(auto4.Zadacha4(), "Po etomy Atrur ne zametil");
+        }
+        [TestMethod]
+        public void TestMethod9()
+        {
+            Starik auto5 = new Starik("Старик");
+            Assert.AreEqual(auto5.name, "Старик");
+        }
+
+        [TestMethod]
+        public void TestMothod10()
+        {
+            Starik auto5 = new Starik("Старик");
+            Assert.AreEqual(auto5.Zadacha5(), "Starika, poka ne stolknulsa s nim");
+        }
+
+    }
+}
+
+
+
+
+   
